fb06aecbcf4d97630a99c695e8bfb77c91a0c096
Leo Koppelkamm <hello@leo-koppelkamm.de>
2015-01-12 16:57:36 +0100

    Updates

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var autoprefixer = require('gulp-autoprefixer');
 var concat = require('gulp-concat');
+var zip = require('gulp-zip');
 var rename = require("gulp-rename");
 var uglify = require("gulp-uglify");
 
@@ -16,7 +17,7 @@ gulp.task('css', function () {
 
 // Combine js
 gulp.task('jsconcat', function () {
-	return gulp.src('src/*.js')
+	return gulp.src(['src/lib/*.js', 'src/*.js'])
 		.pipe(concat('app.js'))
 		.pipe(gulp.dest('dist'));
 });
@@ -29,16 +30,23 @@ gulp.task('jsmin', function () {
 		.pipe(gulp.dest('dist'));
 });
 
+// Copy html and assets to dist
+gulp.task('copy', function () {
+	return gulp.src(['src/index.html', 'src/assets/*'])
+		.pipe(gulp.dest('dist'));
+});
+
 // Watch
 gulp.task('default', function () {
+	gulp.start('css', 'jsconcat', 'jsmin', 'copy');
 	gulp.watch('src/**/*.css', ['css']);
 	gulp.watch('src/**/*.js', ['jsconcat', 'jsmin']);
 });
 
 // Package
 gulp.task('zip', function () {
-	gulp.run('css', 'jsconcat', 'jsmin');
-	gulp.src('dist/*', 'other/*')
+	gulp.start('css', 'jsconcat', 'jsmin', 'copy');
+	gulp.src(['dist/*', 'other/*'])
 		.pipe(zip('Cover.zip'))
-		.pipe(gulp.dest('dist'));
+		.pipe(gulp.dest(''));
 });
