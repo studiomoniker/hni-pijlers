406f446da55bb97eb8468183de6f051fc960dde5
Leo Koppelkamm <hello@leo-koppelkamm.de>
2015-01-21 13:17:09 +0100

    Back to manual

--- src/lib/getData.js
+++ src/lib/getData.js
@@ -1,23 +1,60 @@
-var getUrlVars = function() {
-    var vars = [], hash;
-    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
-    for(var i = 0; i < hashes.length; i++) {
-        hash = hashes[i].split('=');
-        vars.push(hash[0]);
-        vars[hash[0]] = hash[1];
+var BASE64 = {
+  // public method for decoding
+  decode: function (input) {
+    return this._utf8_decode(atob(input));
+  },
+
+  // private method for UTF-8 decoding
+  _utf8_decode: function (utftext) {
+    var string = "";
+    var i = 0;
+    var c = 0,
+      c1 = 0,
+      c2 = 0;
+
+    while (i < utftext.length) {
+
+      c = utftext.charCodeAt(i);
+
+      if (c < 128) {
+        string += String.fromCharCode(c);
+        i++;
+      } else if ((c > 191) && (c < 224)) {
+        c2 = utftext.charCodeAt(i + 1);
+        string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
+        i += 2;
+      } else {
+        c2 = utftext.charCodeAt(i + 1);
+        c3 = utftext.charCodeAt(i + 2);
+        string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
+        i += 3;
+      }
     }
-    return vars;
+    return string;
+  }
+};
+
+var getUrlVars = function () {
+  var vars = [],
+    hash;
+  var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+  for (var i = 0; i < hashes.length; i++) {
+    hash = hashes[i].split('=');
+    vars.push(hash[0]);
+    vars[hash[0]] = hash[1];
+  }
+  return vars;
 };
 
 var urlVars = getUrlVars();
 if (typeof urlVars.title !== 'undefined') {
-  var coverTitle = atob(urlVars.title);
+  var coverTitle = Base64.decode(urlVars.title);
 } else {
   var coverTitle = 'Title was not passed';
 }
 
 if (typeof urlVars.subtitle !== 'undefined') {
-  var coverSubtitle = atob(urlVars.subtitle);
+  var coverSubtitle = Base64.decode(urlVars.subtitle);
 } else {
-  var coverSubtitle = 'Subtitle was not passed';
+  var coverSubtitle = '';
 }
