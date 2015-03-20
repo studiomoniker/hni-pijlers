59c834b53cee341de176801b38554afb0352f176
Leo Koppelkamm <hello@leo-koppelkamm.de>
2015-03-20 15:14:23 +0100

    window.postmessage

--- src/app.js
+++ src/app.js
@@ -1,2 +1,12 @@
-document.write(coverTitle);
-document.write(coverSubtitle);
+getTitleData(function (msg) {
+  document.querySelector('h1').innerHTML = msg.title;
+  document.querySelector('h2').innerHTML = msg.subtitle;
+  
+  // Add link to body to have the parent redirect to the magazine URL
+  if (msg.isHomepage) {
+    document.body.addEventListener('click', function() {
+      redirectParent(msg.domain_path);
+    });
+    document.body.style.cursor = 'pointer';
+  }
+});
--- src/lib/getData.js
+++ src/lib/getData.js
@@ -1,60 +1,27 @@
-var Base64 = {
-  // public method for decoding
-  decode: function (input) {
-    return this._utf8_decode(atob(input));
-  },
-
-  // private method for UTF-8 decoding
-  _utf8_decode: function (utftext) {
-    var string = "";
-    var i = 0;
-    var c = 0,
-      c1 = 0,
-      c2 = 0;
-
-    while (i < utftext.length) {
-
-      c = utftext.charCodeAt(i);
-
-      if (c < 128) {
-        string += String.fromCharCode(c);
-        i++;
-      } else if ((c > 191) && (c < 224)) {
-        c2 = utftext.charCodeAt(i + 1);
-        string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
-        i += 2;
-      } else {
-        c2 = utftext.charCodeAt(i + 1);
-        c3 = utftext.charCodeAt(i + 2);
-        string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
-        i += 3;
-      }
-    }
-    return string;
-  }
-};
-
-var getUrlVars = function () {
-  var vars = [],
-    hash;
-  var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
-  for (var i = 0; i < hashes.length; i++) {
-    hash = hashes[i].split('=');
-    vars.push(hash[0]);
-    vars[hash[0]] = hash[1];
-  }
-  return vars;
-};
-
-var urlVars = getUrlVars();
-if (typeof urlVars.title !== 'undefined') {
-  var coverTitle = Base64.decode(urlVars.title);
-} else {
-  var coverTitle = 'Title was not passed';
+function redirectParent(url) {
+  parent.postMessage('{"status": "redirect", "domain_path": "' + url + '"}', '*');
 }
 
-if (typeof urlVars.subtitle !== 'undefined') {
-  var coverSubtitle = Base64.decode(urlVars.subtitle);
-} else {
-  var coverSubtitle = '';
+function getTitleData(callback) {
+
+  // Somehow determines if we're on the homepage. no idea what they came up here...
+  function isHomepage() {
+    var match = RegExp('[?&]homepage=([^&]*)').exec(window.location.search);
+    return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
+  }
+
+  // Receive data having title and subtitle and put in on the page
+  window.addEventListener('message', function (e) {
+    var msg = JSON.parse(e.data);
+    if (msg.domain_path !== undefined) {
+      msg.isHomepage = isHomepage();
+      callback(msg);
+    }
+  });
+
+  // Create unique cover id
+  var coverId = (window.location.pathname).split('/')[4];
+
+  // Send information to parent saying that loading of the iframe is ready
+  parent.postMessage('{"status": "ready", "coverId": "' + coverId + '"}', '*');
 }
