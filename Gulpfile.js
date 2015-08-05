f771dd76e76727eb42386ff5eceb9c58ca94bf57
Jonathan Puckey <me@jonathanpuckey.com>
2015-08-05 13:28:34 +0200

    tabs -> spaces

--- Gulpfile.js
+++ Gulpfile.js
@@ -14,33 +14,33 @@ gulp.task('css', function () {
     .pipe(sass({
       errLogToConsole: true
     }))
-		.pipe(autoprefixer({
-			browsers: ['last 2 versions'],
-			cascade: false
-		}))
-		.pipe(gulp.dest('dist'));
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(gulp.dest('dist'));
 });
 
 // Combine js
 gulp.task('js', function () {
-	return gulp.src(['src/js/lib/*.js', 'src/js/app.js'])
-		.pipe(concat('app.js'))
-		.pipe(gulp.dest('dist'))
-		.pipe(rename('app-min.js'))
-		.pipe(uglify())
-		.pipe(gulp.dest('dist'));
+  return gulp.src(['src/js/lib/*.js', 'src/js/app.js'])
+    .pipe(concat('app.js'))
+    .pipe(gulp.dest('dist'))
+    .pipe(rename('app-min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist'));
 });
 
 gulp.task('html', function() {
-	return gulp.src('src/index.html')
-	.pipe(inlinesource())
-	.pipe(gulp.dest('dist'));
+  return gulp.src('src/index.html')
+  .pipe(inlinesource())
+  .pipe(gulp.dest('dist'));
 })
 
 // Copy html and assets to dist
 gulp.task('copy', function () {
-	return gulp.src(['src/assets/**/**'], {base: 'src/'})
-		.pipe(gulp.dest('dist'));
+  return gulp.src(['src/assets/**/**'], {base: 'src/'})
+    .pipe(gulp.dest('dist'));
 });
 
 // All except zip
@@ -48,9 +48,9 @@ gulp.task('all', ['copy', 'html', 'js', 'css']);
 
 // Watch
 gulp.task('watch', function () {
-	gulp.watch('./src/*.html', ['html']);
-	gulp.watch('./src/*.css', ['css']);
-	gulp.watch('./src/*.js', ['html', 'js']);
+  gulp.watch('./src/*.html', ['html']);
+  gulp.watch('./src/*.css', ['css']);
+  gulp.watch('./src/*.js', ['html', 'js']);
 });
 
 // Default
@@ -58,15 +58,15 @@ gulp.task('default', ['all', 'watch']);
 
 // Push to gh-pages
 gulp.task('deploy', function () {
-	return gulp.src('./dist/**/*')
-		.pipe(ghpages());
+  return gulp.src('./dist/**/*')
+    .pipe(ghpages());
 });
 
 // Package
 gulp.task('package', ['all', 'zip']);
 
 gulp.task('zip', function () {
-	gulp.src(['src/**', 'dist/*', 'other/*'])
-		.pipe(zip('Cover.zip'))
-		.pipe(gulp.dest(''));
+  gulp.src(['src/**', 'dist/*', 'other/*'])
+    .pipe(zip('Cover.zip'))
+    .pipe(gulp.dest(''));
 });
