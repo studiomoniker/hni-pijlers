36e519b201149230c8a688e65adf3ba06deb77ab
Jonathan Puckey <me@jonathanpuckey.com>
2016-03-12 18:35:10 +0100

    file loader for fonts

--- webpack.config.js
+++ webpack.config.js
@@ -35,6 +35,10 @@ module.exports = {
         {
            test: /index\.html$/,
            loader: extractHtml.extract('html')
+        },
+        {
+          test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+          loader: 'file-loader'
         }
       ]
    },
